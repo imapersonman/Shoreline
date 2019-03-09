a39d928392837121d7dc412df4f049c548941da9
Koissi Adjorlolo <imapersonman@koissis-macbook-air.local>
2019-03-09 15:39:10 -0500

    Initial Commit

---
+++ Shoreline/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shoreline
+//
+//  Created by Koissi Adjorlolo on 3/9/19.
+//  Copyright © 2019 Koissi Adjorlolo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
